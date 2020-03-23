2c91aff5c8ae3d50a872772cc2240a8cea3fa835
johncosta313 <johncosta313@gmail.com>
2020-03-23 16:31:03 -0400

    csvreader

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,3 +9,10 @@ value = 0
 change = 0
 dates = []
 profits = []
+
+with open(budget_data, newline = "") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+
+    csv_header = next(csvreader)
+
+    
